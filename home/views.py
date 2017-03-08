cbd0626bcb6bf927964cfd6288fa4cf5ce035985
Alexandr Monastyretsky <alex.mon1989@gmail.com>
2017-03-08 21:57:51 +0200

    feature: ограничение доступа.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from decorators import group_required
 
 
+@login_required
+@group_required('Users')
 def dashboard(request):
     """Shows dashboard page."""
     return render(request, 'home/dashboard.html')
--- search/views.py
+++ search/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django_tables2 import RequestConfig
 from .forms import SearchForm
 from home.models import House
 from listings.tables import NewListingsTableWithPhoto
 
+from decorators import group_required
 
+
+@login_required
+@group_required('Users')
 def index(request):
     """Shows search page and search results."""
     houses = []
