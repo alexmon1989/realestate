06e7c3dd550a4acfa088006f4c6ff511ae39c230
alexmon1989 <alex.mon1989@gmail.com>
2017-06-29 13:55:15 +0300

    feature: изменение адреса админ-панели.

--- realestate/urls.py
+++ realestate/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^listings/', include('listings.urls')),
     url(r'^search/', include('search.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^siberia/', admin.site.urls),
 ]
 
 if settings.DEBUG:
